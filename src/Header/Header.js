a3317a22e003ba0909c8feb7047f8088afd7ae5a
Horingring <dldldksl@naver.com>
2020-03-18 14:51:17 +0900

    0318_2__FacebookClonePrac_3__Header_1

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component,Fragment} from 'react';
 import './Header.css';
 import HeaderLogo from './HeaderLogo';
 import HeaderSearchBar from './HeaderSearchBar';
@@ -7,9 +7,16 @@ import HeaderSettingBox from './HeaderSettingBox';
 
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mode : 'off'
+        }
+    }
 
     render(){
         return (
+            <Fragment>
             <div className="headerMain">
                 <div className="div100"></div>
                 <HeaderLogo></HeaderLogo>
@@ -17,9 +24,18 @@ class Header extends Component {
                 <HeaderSearchBar></HeaderSearchBar>
                 <div className="div400"></div>
                 <HeaderQuickBar></HeaderQuickBar>
-                <div className="div100"></div>
-                <HeaderSettingBox></HeaderSettingBox>
+                <div className="div300_right"></div>
+                <HeaderSettingBox 
+                    mode={this.state.mode}
+                    onChangeMode={function(_mode){
+                        this.setState({
+                            mode : _mode
+                        });
+                    }.bind(this)}> 
+                </HeaderSettingBox>
             </div>
+            <div className="testDivForScroll"></div>
+            </Fragment>
         );
     }
 }
--- src/Header/HeaderSettingBox.js
+++ src/Header/HeaderSettingBox.js
@@ -1,45 +1,45 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import triangle from './HeaderImg/triangle.png'
 
 class HeaderSettingBox extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            mode : 'off'
-        }
-    }
-
     render(){
+
         let settingBox = null;
-        if(this.state.mode === 'on'){
+        if(this.props.mode === 'on'){
             settingBox = (
                 <div id="headerSettingBox">
-                    <div class="headerSettingBoxMember">내 그룹</div>
-                    <hr class="hrLine"></hr>
-                    <div class="headerSettingBoxMember">Facebook에서 광고하기</div>
-                    <hr class="hrLine"></hr>
-                    <div class="headerSettingBoxMember">활동로그</div>
-                    <div class="headerSettingBoxMember">뉴스피드 기본설정</div>
-                    <div class="headerSettingBoxMember">설정</div>
-                    <hr class="hrLine"></hr>
-                    <div class="headerSettingBoxMember">로그아웃</div>
+                    <div className="headerSettingBoxMember">내 그룹</div>
+                    <hr className="hrLine"></hr>
+                    <div className="headerSettingBoxMember">Facebook에서 광고하기</div>
+                    <hr className="hrLine"></hr>
+                    <div className="headerSettingBoxMember">활동로그</div>
+                    <div className="headerSettingBoxMember">뉴스피드 기본설정</div>
+                    <div className="headerSettingBoxMember">설정</div>
+                    <hr className="hrLine"></hr>
+                    <div className="headerSettingBoxMember">로그아웃</div>
                 </div>
             );
-        }else if(this.state.mode === 'off'){
+        }else if(this.props.mode === 'off'){
             settingBox = null;
         }
+
         return (
-            <div 
-              className="headerMember" 
-              id="headerSettingBoxBtn" 
-              onClick={function(){
-                this.setState({
-                    mode : 'on'
-                });
-              }.bind(this)}>
-                <img src={triangle} alt="셋팅박스 아이콘"></img>
+            <Fragment>
+                <div 
+                className="headerMember" 
+                id="headerSettingBoxBtn" 
+                onClick={function(){
+                    let _mode = null;
+                    this.props.mode === 'on'
+                        ? _mode = 'off'
+                        : _mode = 'on' 
+                    this.props.onChangeMode(_mode);
+                }.bind(this)}>
+                    <img src={triangle} alt="셋팅박스 아이콘"></img>
+                    
+                </div>
                 {settingBox}
-            </div>
+            </Fragment>
         );
     }
 }
